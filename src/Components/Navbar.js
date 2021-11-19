dbf55e1589d03603faf539870279e9576c8a67a9
Shreyas Shettigar <shreyasshettigar34@gmail.com>
2021-11-20 01:20:56 +0530

    npm heroicons and bootstrap

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -2,18 +2,19 @@ import React from "react";
 import {
   SearchIcon,
   CurrencyRupeeIcon,
-  //   UsersIcon,
+  UsersIcon,
   MenuIcon,
   UserCircleIcon,
-} from "@heroicons/react";
+} from "@heroicons/react/solid";
 const Navbar = () => {
   return (
     <div>
       <header className="sticky-top z-50  bg-white  p-3">
-        <div className="row"></div>
+        <div className="row">
+          <CurrencyRupeeIcon />
+        </div>
         <div className="relative flex items-center h-10 cursor-pointer my-auto ">
           <img src="" alt="" srcset="" />
-          <button className="btn btn"></button>
         </div>
 
         <div className="flex items-center md:border-2 py-2 rounded-full">
@@ -21,15 +22,15 @@ const Navbar = () => {
             type="text"
             // value={searchInput}
             // onChange={(e) => setSearchInput(e.target.value)}
-            className="text-gray-600 text-sm pl-5 bg-transparent outline-none flex-grow "
+            className=""
             placeholder="Explore more"
           />
-          <SearchIcon className="hidden md:inline-block h-8 bg-blue-400  text-white rounded-full p-2 cursor-pointer md:mx-2" />
+          <SearchIcon className="rounded-full p-2 cursor-pointer " />
         </div>
 
         <div className="flex items-center cursor-pointer space-x-2 justify-end opacity-60">
           <p className="font-sans hidden md:inline-block">Buy me a coffee</p>
-          <CurrencyRupeeIcon className="h-6" />
+          <CurrencyRupeeIcon className="" height="20px" />
 
           <div className="flex items-center border-2 shadow-md space-x-2 p-2 rounded-full">
             <MenuIcon className="h-6" />
