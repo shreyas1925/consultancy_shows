8c18a7a8874c39dc91c617ecaf7ea7e399f9c0b0
Shreyas Shettigar <shreyasshettigar34@gmail.com>
2021-11-20 23:10:18 +0530

    adding classes

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -11,30 +11,29 @@ const Navbar = () => {
     <div>
       <header className="sticky-top z-50  bg-white  p-3">
         <div className="row">
-          <CurrencyRupeeIcon />
-        </div>
-        <div className="relative flex items-center h-10 cursor-pointer my-auto ">
-          <img src="" alt="" srcset="" />
-        </div>
+          <div className="col-sm-3 col-md-4 col-12 ">
+            <img src="" alt="" srcset="" />
+          </div>
 
-        <div className="flex items-center md:border-2 py-2 rounded-full">
-          <input
-            type="text"
-            // value={searchInput}
-            // onChange={(e) => setSearchInput(e.target.value)}
-            className=""
-            placeholder="Explore more"
-          />
-          <SearchIcon className="rounded-full p-2 cursor-pointer " />
-        </div>
+          <div className="col-sm-3 col-md-4 col-12">
+            <input
+              type="text"
+              // value={searchInput}
+              // onChange={(e) => setSearchInput(e.target.value)}
+              className=""
+              placeholder="Explore more"
+            />
+            <SearchIcon className="icon" />
+          </div>
 
-        <div className="flex items-center cursor-pointer space-x-2 justify-end opacity-60">
-          <p className="font-sans hidden md:inline-block">Buy me a coffee</p>
-          <CurrencyRupeeIcon className="" height="20px" />
+          <div className="col-sm-3 col-md-4 col-12">
+            <p className="">Buy me a coffee</p>
+            <CurrencyRupeeIcon className="icon" height="20px" />
 
-          <div className="flex items-center border-2 shadow-md space-x-2 p-2 rounded-full">
-            <MenuIcon className="h-6" />
-            <UserCircleIcon className="h-6" />
+            <div className="">
+              <MenuIcon className="icon" />
+              <UserCircleIcon className="icon" />
+            </div>
           </div>
         </div>
       </header>
