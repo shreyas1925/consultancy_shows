393e7732f06c0754dc95320fa9f36e25f1edca09
Shreyas Shettigar <shreyasshettigar34@gmail.com>
2021-11-24 00:52:17 +0530

    added 2 features of carousel

--- src/Components/Advertisement.js
+++ src/Components/Advertisement.js
@@ -3,13 +3,22 @@ import one from "../assets/images/one.jpg";
 
 const Advertisement = () => {
   return (
-    <div>
+    <div
+      style={{
+        display: "block",
+        marginLeft: "auto",
+        marginRight: "auto",
+        width: "80%",
+        marginTop: "40px",
+        marginBottom: "40px",
+      }}
+    >
       <img
         src={one}
         alt=""
         srcset=""
         className="img-fluid"
-        style={{ width: "1300px", height: "220px" }}
+        style={{ width: "1300px", height: "280px" }}
       />
     </div>
   );
---
+++ src/Components/Extras.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Extras = () => {
+  return (
+    <div className="grid">
+      <div>
+        <p>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vel, at?
+        </p>
+      </div>
+      <div>
+        <p>Lorem ipsum dolor sit amet.</p>
+      </div>
+    </div>
+  );
+};
+
+export default Extras;
