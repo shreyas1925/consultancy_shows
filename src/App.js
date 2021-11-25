2dfac70fa47b28aaa879884e12e79cc8045f3fec
Shreyas Shettigar <shreyasshettigar34@gmail.com>
2021-11-25 23:46:06 +0530

    setting up register and login page

--- src/App.js
+++ src/App.js
@@ -1,16 +1,24 @@
 import React from "react";
+import { Route, Routes } from "react-router-dom";
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 import { BrowserRouter as Router } from "react-router-dom";
 import HomeScreen from "./screens/HomeScreen";
+import RegisterScreen from "./screens/RegisterScreen";
+import LoginScreen from "./screens/LoginScreen";
+
 const App = () => {
   return (
-    <div className="app">
+    <>
+      {/* <div className="app"> */}
       <Router>
-        <HomeScreen />
+        <Route component={RegisterScreen} />
+        <Route path="/login" component={LoginScreen} />
       </Router>
+
       {/* <h1>Hello Consulators</h1> */}
-    </div>
+      {/* </div> */}
+    </>
   );
 };
 
