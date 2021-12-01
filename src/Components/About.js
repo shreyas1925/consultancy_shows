d1eafb23162634eaff731e5304fcfb04874cdf43
Shreyas Shettigar <shreyasshettigar34@gmail.com>
2021-12-01 23:45:14 +0530

    adding about section

--- src/Components/About.js
+++ src/Components/About.js
@@ -4,10 +4,137 @@ import "./About.css";
 const About = () => {
   return (
     <>
+      <h1>What all we provide</h1>
+      <hr />
+      <div className="">
+        <div className="card" style={{ width: "30rem" }}>
+          <img
+            className="card-img-top"
+            alt=""
+            src="https://img.icons8.com/ios-glyphs/50/000000/man-on-phone.png"
+          />
+          <div className="card-body">
+            <h3 className="card-title">Choose a Service</h3>
+            <p className="card-text">
+              Businesses often need different solutions. Draw from our portfolio
+              of advanced technological services, that suit your needs.
+            </p>
+            {/* <Link to="#" className="btn btn-primary">
+              Go somewhere
+            </Link> */}
+          </div>
+        </div>
+        <div className="card" style={{ width: "30rem" }}>
+          <img
+            className="card-img-top"
+            alt=""
+            src="https://img.icons8.com/ios-glyphs/50/000000/man-on-phone.png"
+          />
+          <div className="card-body">
+            <h3 className="card-title">Choose a Service</h3>
+            <p className="card-text">
+              Businesses often need different solutions. Draw from our portfolio
+              of advanced technological services, that suit your needs.
+            </p>
+            {/* <Link to="#" className="btn btn-primary">
+              Go somewhere
+            </Link> */}
+          </div>
+        </div>
+        <div className="card" style={{ width: "30rem" }}>
+          <img
+            className="card-img-top"
+            alt=""
+            src="https://img.icons8.com/ios-glyphs/50/000000/man-on-phone.png"
+          />
+          <div className="card-body">
+            <h3 className="card-title">Choose a Service</h3>
+            <p className="card-text">
+              Businesses often need different solutions. Draw from our portfolio
+              of advanced technological services, that suit your needs.
+            </p>
+            {/* <Link to="#" className="btn btn-primary">
+              Go somewhere
+            </Link> */}
+          </div>
+        </div>
+        <div className="card" style={{ width: "30rem" }}>
+          <img
+            className="card-img-top"
+            alt=""
+            src="https://img.icons8.com/ios-glyphs/50/000000/man-on-phone.png"
+          />
+          <div className="card-body">
+            <h3 className="card-title">Choose a Service</h3>
+            <p className="card-text">
+              Businesses often need different solutions. Draw from our portfolio
+              of advanced technological services, that suit your needs.
+            </p>
+            {/* <Link to="#" className="btn btn-primary">
+              Go somewhere
+            </Link> */}
+          </div>
+        </div>
+      </div>
+
       <h1 className="text-center mt-5 ">How we work</h1>
       <hr />
 
       <div className="d-flex align-items-center justify-content-center">
+        <div className="card" style={{ width: "30rem" }}>
+          <img
+            className="card-img-top"
+            alt=""
+            src="https://img.icons8.com/ios-glyphs/50/000000/man-on-phone.png"
+          />
+          <div className="card-body">
+            <h3 className="card-title">Choose a Service</h3>
+            <p className="card-text">
+              Businesses often need different solutions. Draw from our portfolio
+              of advanced technological services, that suit your needs.
+            </p>
+            {/* <Link to="#" className="btn btn-primary">
+              Go somewhere
+            </Link> */}
+          </div>
+        </div>
+
+        <div className="card" style={{ width: "30rem" }}>
+          <img
+            className="card-img-top"
+            alt=""
+            src="https://img.icons8.com/ios-glyphs/50/000000/man-on-phone.png"
+          />
+          <div className="card-body">
+            <h3 className="card-title">Choose a Service</h3>
+            <p className="card-text">
+              Businesses often need different solutions. Draw from our portfolio
+              of advanced technological services, that suit your needs.
+            </p>
+            {/* <Link to="#" className="btn btn-primary">
+              Go somewhere
+            </Link> */}
+          </div>
+        </div>
+
+        <div className="card" style={{ width: "30rem" }}>
+          <img
+            className="card-img-top"
+            alt=""
+            src="https://img.icons8.com/ios-glyphs/50/000000/man-on-phone.png"
+          />
+          <div className="card-body">
+            <h3 className="card-title">Choose a Service</h3>
+            <p className="card-text">
+              Businesses often need different solutions. Draw from our portfolio
+              of advanced technological services, that suit your needs.
+            </p>
+            {/* <Link to="#" className="btn btn-primary">
+              Go somewhere
+            </Link> */}
+          </div>
+        </div>
+
         <div className="card" style={{ width: "30rem" }}>
           <img
             className="card-img-top"
