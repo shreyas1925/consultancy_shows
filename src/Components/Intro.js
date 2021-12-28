e75be29d648bcf8eb5bd4a4582c0dd5832df61ec
Shreyas Shettigar <shreyasshettigar34@gmail.com>
2021-12-28 23:16:13 +0530

    dealing with info

--- src/Components/Intro.js
+++ src/Components/Intro.js
@@ -34,7 +34,12 @@ const Intro = () => {
       <div className="provide">
         <div className="text text-center card_parent">
           <h1>PROCESS</h1>
-          <h2>How we work 👨‍💻</h2>
+          <h2>
+            How we work{" "}
+            <span>
+              <img src="https://img.icons8.com/external-parzival-1997-outline-color-parzival-1997/64/000000/external-work-worklife-balance-and-stress-management-parzival-1997-outline-color-parzival-1997.png" />
+            </span>
+          </h2>
         </div>
 
         <div className="container-fluid mb-5 mt-5 ">
@@ -42,8 +47,10 @@ const Intro = () => {
             <div className="col-lg-3 col-md-6 col-12 ">
               <div className="card">
                 <div className="contents">
-                  <div className="icon">Lorem, ipsum dolor.</div>
-                  <h2>Create a Service</h2>
+                  <div className="icon">
+                    <img src="https://img.icons8.com/color/48/000000/globe--v1.png" />
+                  </div>
+                  <h2>Explore</h2>
                   <p className="card_para">
                     Lorem ipsum dolor sit, amet consectetur adipisicing elit. In
                     distinctio cumque consectetur, omnis ab magni vel atque
@@ -55,7 +62,9 @@ const Intro = () => {
             <div className="col-lg-3 col-md-6 col-12 ">
               <div className="card">
                 <div className="contents">
-                  <div className="icon">Lorem, ipsum dolor.</div>
+                  <div className="icon">
+                    <img src="https://img.icons8.com/color/48/000000/service.png" />
+                  </div>
                   <h2>Filter services</h2>
                   <p className="card_para">
                     Lorem ipsum dolor sit, amet consectetur adipisicing elit. In
@@ -68,7 +77,9 @@ const Intro = () => {
             <div className="col-lg-3 col-md-6 col-12 ">
               <div className="card">
                 <div className="contents">
-                  <div className="icon">Lorem, ipsum dolor.</div>
+                  <div className="icon">
+                    <img src="https://img.icons8.com/external-becris-lineal-color-becris/48/000000/external-meeting-business-management-becris-lineal-color-becris.png" />
+                  </div>
                   <h2>Request a Meeting</h2>
                   <p className="card_para">
                     Lorem ipsum dolor sit, amet consectetur adipisicing elit. In
@@ -81,7 +92,9 @@ const Intro = () => {
             <div className="col-lg-3 col-md-6 col-12 ">
               <div className="card">
                 <div className="contents">
-                  <div className="icon">Lorem, ipsum dolor.</div>
+                  <div className="icon">
+                    <img src="https://img.icons8.com/color/48/000000/add-user-group-woman-woman.png" />
+                  </div>
                   <h2>Connect </h2>
                   <p className="card_para">
                     Lorem ipsum dolor sit, amet consectetur adipisicing elit. In
